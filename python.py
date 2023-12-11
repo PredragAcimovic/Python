658ca993e99cb0c056b626676a7d9447a88c352e
Predrag <pedjaacimovic44@gmail.com>
2023-12-11 18:17:10 +0100

    imenovanje i koriscenje varijabli

--- python.py
+++ python.py
@@ -3,3 +3,8 @@ print("Hello Python World")
 #varijable
 message = "Hello"
 print(message)
+message = "Hello Python Crash Course world!"
+print (message)
+#imenovanje i koriscenje varijabli
+message = "Hello Python Crash Course reader!"
+print(message)
