65562b65acdf61210fffcc02c76479d3fcabc9cf
Predrag <pedjaacimovic44@gmail.com>
2023-12-12 18:07:31 +0100

    prvi zadatak

--- python.py
+++ python.py
@@ -8,3 +8,8 @@ print (message)
 #imenovanje i koriscenje varijabli
 message = "Hello Python Crash Course reader!"
 print(message)
+#1-1
+message = "first assigment"
+print(message)
+message = "first assigment 2"
+print(message)
